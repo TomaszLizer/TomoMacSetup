67abf5d76c4ce5a93b97ec479ca9bb0f55fa0f15
Tomasz Lizer <tomasz.lizer@brightinventions.pl>
2023-08-28 10:53:57 +0200

    update setup script

--- setupMac.sh
+++ setupMac.sh
@@ -3,12 +3,9 @@ echo "Setting up your new Mac ;>"
 #setup console
 git clone https://github.com/zdharma/fast-syntax-highlighting.git ~/.zsh/plugins/fast-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.zsh/plugins/zsh-autosuggestions
-
+cp console/completion.zsh ~/.zsh/plugins/
 
 # ============== Installing Apps ==============
-
-# installing rvm with ruby
-\curl -sSL https://get.rvm.io | bash -s stable --ruby
 
 # Setting up Homebrew
 if test ! $(which brew); then
@@ -19,64 +16,63 @@ else
     brew update
 fi
 
-# Installing brew packages
-brew install mas #tool to install mac app store apps from command line
-brew install starship #make cosnole beautifull :)
-
-# Avio speciffic
-brew install xcodegen
-
-caskWorkspaceApps=(
-    fork
-    iterm2
-    isimulator
-    postman
-    charles
-    visual-studio-code
+cliApps=(
+    starship # make console beautiful :)
+    rbenv
+    jenv
+    pyenv
+    swiftlint
+    swift-format
+    peripheryapp/periphery/periphery # Swift dead code analysis
 )
+
+# Installing brew cli tools
+echo "Installing cli apps with brew"
+brew install --appdir="/Applications" ${cliApps[@]}
 
 caskApps=(
-    zoomus
+    fork # Best git GUI ever
+    slack
+    iterm2 # 
+    isimulator # iOS simulator menu bar helper
+    postman
+    itsycal # menu bar calendar
+    setapp
+    visual-studio-code
     google-chrome
-    tunnelblick
-)
-
-masApps=(
-    640199958 # Apple Developer
-    497799835 # Xcode
-    803453959 # Slack
 )
 
 # Installing apps with brew cask
 echo "Installing apps with brew cask"
-brew install --cask --appdir="/Applications/Work" ${caskWorkspaceApps[@]}
 brew install --cask --appdir="/Applications" ${caskApps[@]}
 
 
 # ============== System settings ==============
 
-#"Showing all filename extensions in Finder by default"
+# Showing all filename extensions in Finder by default
 defaults write NSGlobalDomain AppleShowAllExtensions -bool true
 
-#"Disabling the warning when changing a file extension"
+# Disabling the warning when changing a file extension
 defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
 
-#Changes Finder to List View
-#Four-letter codes for the other view modes: 'icnv', 'clmv', 'Flwv'
+# Changes Finder to List View
+# Four-letter codes for the other view modes: 'icnv', 'clmv', 'Flwv'
 defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
 
-#Shows Status Bar
+# Shows Status Bar
 defaults write com.apple.finder ShowStatusBar -bool true;
 
-#Shows Path Bar
+# Shows Path Bar
 defaults write com.apple.finder ShowPathbar -bool true;
 
-#New Finder windows now opens in /Users/<username>
+# New Finder windows now opens in /Users/<username>
 defaults write com.apple.finder NewWindowTarget -string "PfHm"
 
 # Show hidden files in Finder
 defaults write com.apple.finder AppleShowAllFiles -bool true
 
+# Stop xcode from opening last used projects - always ask what to open
+defaults write com.apple.dt.Xcode ApplePersistenceIgnoreState -bool YES
 
 # ============== Prepare system ==============
 
